b5ea21d2c28e6b80c460a8ef45728d04cc188530
Emil Engberg <emil.engberg@outlook.com>
2021-09-25 01:14:23 +0200

    Chaged to Newtons method

--- main.cpp
+++ main.cpp
@@ -1,32 +1,25 @@
 #include <stdio.h>
-#include <gmp.h>
 #include <iostream>
 #include <cmath>
+#include <boost/multiprecision/mpfr.hpp>
+#include <boost/multiprecision/gmp.hpp> 
+using namespace boost::multiprecision;
 using namespace std;
 int main()
 {
-  mpz_t a, b, a_temp, b_temp,x,b2,a2;
-  mpz_init_set_ui(a,3);
-  mpz_init_set_ui(x,3);
-  mpz_init_set_ui(b,2);
-  mpz_init_set_ui(b2,0);
-  mpz_init_set_ui(a2,0);
-  mpz_init_set_ui(a_temp,3);
-  mpz_init_set_ui(b_temp,2);
-  for(int i=0; i < 10;i++){
-    mpz_set(a_temp,a);
-    mpz_set(b_temp,b);
-    mpz_mul_ui(a,a,4);
-    mpz_mul(a,a,b);
-    mpz_pow_ui(a2,a_temp,2);
-    mpz_pow_ui(b2,b,2);
-    mpz_mul_ui(b2,b2,2);
-    mpz_add(b,a2,b2);
-    mpz_gcd(x,a,b);
-    mpz_divexact(a,a,x);
-    mpz_divexact(b,b,x);
-    cout << a << " " << b << "\n";
+  long long int n = 1000000;
+  cout.precision(n+2);
+  mpfr_float::default_precision(n);
+  mpfr_float a_old=0;
+  mpfr_float a = 1.5;
+  int i = 0;
+  while(a!=a_old){
+    a_old=a;
+    a=(2/a+a)/2;
+    i++;
+    cout << i << "\n";
   }
+  // std::cout << a << std::endl;
   return 0;
    
 }
