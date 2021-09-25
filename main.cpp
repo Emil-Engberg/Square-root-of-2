8927aef65f0cde4960837d023c28697b281610e2
Emil Engberg <emil.engberg@outlook.com>
2021-09-25 02:08:45 +0200

    Made code multithreaded

--- main.cpp
+++ main.cpp
@@ -1,25 +1,41 @@
 #include <stdio.h>
+#include <fstream>
 #include <iostream>
 #include <cmath>
+#include <future>
 #include <boost/multiprecision/mpfr.hpp>
 #include <boost/multiprecision/gmp.hpp> 
 using namespace boost::multiprecision;
 using namespace std;
+
+mpz_int calc_num(mpz_int a, mpz_int b) {
+  return a*a+2*b*b;
+}
+
+mpz_int calc_dem(mpz_int a, mpz_int b) {
+  return 2*a*b;
+}
+
 int main()
 {
-  long long int n = 1000000;
-  cout.precision(n+2);
-  mpfr_float::default_precision(n);
-  mpfr_float a_old=0;
-  mpfr_float a = 1.5;
-  int i = 0;
-  while(a!=a_old){
-    a_old=a;
-    a=(2/a+a)/2;
-    i++;
+  fstream num, dem;
+  num.open("num.txt",ios::out);
+  dem.open("dem.txt",ios::out);
+  mpz_int a = 3;
+  mpz_int b = 2;
+  mpz_int a_temp;
+  for(int i=0;i<35;i++ ){
+    a_temp = a;
+    future<mpz_int> futnum =  async(calc_num, a,b);
+    future<mpz_int> futdem =  async(calc_dem,a_temp,b);
+    a = futnum.get();
+    b = futdem.get();
     cout << i << "\n";
   }
-  // std::cout << a << std::endl;
+  num << a << std::endl;
+  dem << b << std::endl;
+  num.close();
+  dem.close();
   return 0;
    
 }
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all:
-	g++ -O2 -o main.out main.cpp -pthread -lgmpxx -lgmp -lmpfr -lgmp
+	g++ -std=c++17 -O2 -o main.out main.cpp -pthread -lgmpxx -lgmp -lmpfr -lgmp
